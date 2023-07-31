6060929e126db36bde9c764fcdc62e3f16c4258f
Kostya <user.name>
2023-07-31 17:52:57 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -18,11 +18,12 @@ const results = document.querySelectorAll('.summary__score');
 let sum = 0;
 
 button.onclick = function (e) {
+    sum = 0
     e.preventDefault();
     globalState.results = 0;
     console.log()
     for (let item of inputs) {
-        if (+item.value < 0 || +item.value > 101 || item.value == '') {
+        if (+item.value < 0 || +item.value > 100 || item.value == '') {
             globalState.error = 'Error! Please enter the number between 0 and 100';
             alert(globalState.error);
             return
